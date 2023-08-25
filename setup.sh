e6fef4c44d65771aa35cc4b035033eb2bddafa50
Zsolt Domjan <domjanzsoo@yahoo.com>
2023-08-25 07:05:02 +0100

    using serve and env named arguments in the script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/env bash
 
+environment="dev"
+serve="false"
+
+while getopts "e:s:" var
+do
+  case "$var" in
+    e) environment=${OPTARG};;
+    s) serve=${OPTARG};;
+  esac
+done
+
 setupBackend() {
-  environment=dev
-
-  if [ ! -z "$1" ]; then
-    environment=$1
-  fi
-
   echo "$(tput setaf 2)--------------------------- Copying $(environment) environment file ----------------------$(tput sgr 0)"
   cp ./kudelasz/.env-${environment} ./kudelasz/.env
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
   echo "$(tput setaf 2)--------------------------- Creating virtual environment ----------------------$(tput sgr 0)"
   pip install --upgrade pip
   pip install virtualenv
   virtualenv venv
   source ./venv/Scripts/activate
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
   echo "$(tput setaf 2)----------------------- Installing dependencies ------------------------------$(tput sgr 0)"
   pip install -r requirements.txt
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
   echo "$(tput setaf 2)---------------------------Running migrations -----------------------------------$(tput sgr 0)"
   python ./kudelasz/manage.py migrate
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
-  echo "$(tput setaf 2)----------------------- Running Django server -------------------------$(tput sgr 0)"
-  python ./kudelasz/manage.py runserver
+  if [[$serve == "true" ]]; then
+    echo "$(tput setaf 2)----------------------- Running Django server -------------------------$(tput sgr 0)"
+    python ./kudelasz/manage.py runserver
+    echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
+  fi
 }
 
 setupFrontend() {
@@ -31,12 +43,15 @@ setupFrontend() {
 
   echo "$(tput setaf 2)------------- Pulling frontend repository ----------------------$(tput sgr 0)"
   cd frontend && git clone git@github.com:domjanzsoo/kudelasz-client.git .
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
   echo "$(tput setaf 2)---------------- Installing dependencies ------------------------$(tput sgr 0)"
   npm install
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 
   echo "$(tput setaf 2)----------------- Building frontend -----------------------------$(tput sgr 0)"
   npm run build
+  echo "$(tput setaf 2)--------------------------- Done ----------------------$(tput sgr 0)"
 }
 
 pullBackend() {
@@ -56,11 +71,5 @@ pullFrontend(){
   git pull origin master
 }
 
-
-setupBackend
 setupFrontend
-
-
-
-
-
+setupBackend
